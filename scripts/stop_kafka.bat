bc83e0e30062fdd4fc8e3e4a6835752866cd634e
maor hornstein <mor.h100@gmail.com>
2024-09-12 15:53:02 +0300

    remove redundant waiting in stop scripts
    Tests done: run the stop script and then immediately the start, validate correct lunching.

--- scripts/stop_kafka.bat
+++ scripts/stop_kafka.bat
@@ -5,12 +5,8 @@ set KAFKA_HOME=C:\kafka
 echo Stopping Kafka broker...
 call "%KAFKA_HOME%\bin\windows\kafka-server-stop.bat"
 
-timeout /t 30 >nul 2>&1
-
 echo Stopping ZooKeeper...
 call "%KAFKA_HOME%\bin\windows\zookeeper-server-stop.bat"
-
-timeout /t 30 >nul 2>&1
 
 echo All services stopped.
 pause
