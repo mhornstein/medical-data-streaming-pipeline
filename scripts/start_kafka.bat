606d0880e3d40525b72735abdc92a3aeddf6267d
maor hornstein <mor.h100@gmail.com>
2024-09-12 16:12:59 +0300

    create topics in start script
    Tests done: run the start script followed by the stop script twice.
    Validate that the start script doesn't fail even if the topics already existed.

--- scripts/start_kafka.bat
+++ scripts/start_kafka.bat
@@ -10,5 +10,11 @@ timeout /t 30 >nul 2>&1
 echo Starting Kafka broker...
 start cmd /k "%KAFKA_HOME%\bin\windows\kafka-server-start.bat %KAFKA_HOME%\config\server.properties"
 
+timeout /t 30 >nul 2>&1
+
+echo Creating Kafka topics...
+call "%KAFKA_HOME%\bin\windows\kafka-topics.bat" --create --if-not-exists --topic medical-entries --bootstrap-server localhost:9092
+call "%KAFKA_HOME%\bin\windows\kafka-topics.bat" --create --if-not-exists --topic processed-entries --bootstrap-server localhost:9092
+
 echo All services started.
 pause
